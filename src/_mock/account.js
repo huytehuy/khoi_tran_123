91de95c68b41a48e43a65cffc03e305db244ec48
huytehuy <phamkhanhhuy1231@gmail.com>
2023-04-28 16:00:59 +0700

    hello

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Huytehuy',
+  email: 'admin@huytehuy',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
